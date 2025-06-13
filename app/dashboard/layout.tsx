4db96933b9639c909c7cdab9ca3221fdc3a99fbc
lautaromateol <lautaromateol@gmail.com>
2025-06-13 20:25:04 -0300

    update: redirect to workspace creation page added

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { getToken } from "@/actions/auth/get-token";
-import { API_URL } from "@/lib/constants";
+import { getWorkspaces } from "@/features/workspaces/actions/get-workspaces";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
@@ -7,10 +7,16 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const token = await getToken();
+  const { token } = await getToken();
 
   if (!token) {
     redirect("/sign-in");
+  }
+
+  const { workspaces } = await getWorkspaces();
+
+  if (workspaces.length === 0) {
+    redirect("/create-workspace");
   }
 
   return <>{children}</>;
