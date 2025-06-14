183ebca10cafb20d9c451604bfdc268acb54a596
lautaromateol <lautaromateol@gmail.com>
2025-06-14 16:35:57 -0300

    add: use confirm hook

---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,56 @@
+"use client"
+import { JSX, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ResponsiveModal } from "@/components/responsive-modal";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export function useConfirm(
+  title: string,
+  message: string,
+): [() => JSX.Element, () => Promise<unknown>] {
+
+  const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null)
+
+  function confirm() {
+    return new Promise((resolve) => setPromise({ resolve }))
+  }
+
+  function handleClose() {
+    setPromise(null)
+  }
+
+  function handleConfirm() {
+    promise?.resolve(true)
+    handleClose()
+  }
+
+  function handleCancel() {
+    promise?.resolve(false)
+    handleClose()
+  }
+
+  function ConfirmationDialog() {
+    return (
+      <ResponsiveModal title="Confirm action" open={promise !== null} onOpenChange={handleClose} visuallyHidden className="p-0">
+        <Card className="shadow-none border-0 size-full">
+          <CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{message}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center gap-x-2">
+              <Button onClick={handleCancel} variant="outline">
+                Cancel
+              </Button>
+              <Button onClick={handleConfirm} variant="destructive">
+                Confirm
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </ResponsiveModal>
+    )
+  }
+
+  return [ConfirmationDialog, confirm]
+}
