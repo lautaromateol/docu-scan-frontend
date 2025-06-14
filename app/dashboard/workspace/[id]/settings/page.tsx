b05da160db661a8fb5e7fbc081d3a21e99ccc670
lautaromateol <lautaromateol@gmail.com>
2025-06-14 16:33:00 -0300

    add: workspace settings page

---
+++ app/dashboard/workspace/[id]/settings/page.tsx
@@ -0,0 +1,27 @@
+import { getWorkspace } from "@/features/workspaces/actions/get-workspace";
+import { DangerZone } from "@/features/workspaces/components/danger-zone";
+import { InviteMembers } from "@/features/workspaces/components/invite-members";
+import { UpdateWorkspace } from "@/features/workspaces/components/update-workspace";
+
+export default async function WorkspaceSettingsPage({ params }: any) {
+  const { id } = await params;
+  const workspace = await getWorkspace(id);
+
+  if (!workspace) {
+    throw new Error();
+  }
+
+  return (
+    <div className="rounded space-y-8">
+      <div className="space-y-0.5">
+        <h1 className="text-2xl font-semibold">Settings</h1>
+        <p className="text-base font-light">
+          Update your workspace name, invite members and more.
+        </p>
+      </div>
+      <UpdateWorkspace workspace={workspace} /> 
+      <InviteMembers workspace={workspace} />
+      <DangerZone workspace={workspace} />
+    </div>
+  );
+}
