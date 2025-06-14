f8247a290a63eea13a28f9065d13f12653d58cac
lautaromateol <lautaromateol@gmail.com>
2025-06-14 01:28:36 -0300

    add: workspace selector component

---
+++ features/workspaces/components/workspace-selector.tsx
@@ -0,0 +1,44 @@
+"use client"
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
+import { Workspace } from "../types";
+import { useWorkspaceId } from "../hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
+
+interface WorkspaceSelectorProps {
+  workspaces: Workspace[];
+}
+
+export function WorkspaceSelector({ workspaces }: WorkspaceSelectorProps) {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+
+  function onValueChange(workspaceId: string) {
+    router.push(`/dashboard/workspace/${workspaceId}`);
+  }
+
+  return (
+    <Select value={workspaceId} onValueChange={onValueChange}>
+      <SelectTrigger>
+        <SelectValue placeholder="Select a workspace" />
+      </SelectTrigger>
+      <SelectContent>
+        {workspaces.map((workspace) => (
+          <SelectItem value={workspace.id} key={workspace.id}>
+            <div className="flex items-center gap-x-4">
+              <div className="flex items-center justify-center rounded-full bg-indigo-100 text-indigo-800 font-medium size-6">
+                {workspace.name.at(0)?.toUpperCase()}
+              </div>
+              <p className="text-sm font-light">{workspace.name}</p>
+            </div>
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
