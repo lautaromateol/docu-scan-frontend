87a13d37f446b7c8b1d0c895c4866c99f9105364
lautaromateol <lautaromateol@gmail.com>
2025-06-14 00:45:01 -0300

    update: prevent clause added to getToken function

--- actions/auth/get-token.ts
+++ actions/auth/get-token.ts
@@ -7,5 +7,7 @@ export async function getToken() {
   const token = cookiesStore.get("token")
   const user = cookiesStore.get("user")
 
+  if(!token || !user) return null
+
   return { token: token?.value, user: JSON.parse(user?.value!) }
 }
