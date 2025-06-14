200308eaa6f7808b97403bd6ab381e629c3c3ae3
lautaromateol <lautaromateol@gmail.com>
2025-06-14 16:47:41 -0300

    update: workspace update schema added

--- features/workspaces/types/index.ts
+++ features/workspaces/types/index.ts
@@ -13,6 +13,20 @@ export const CreateWorkspaceObject = z.object({
 
 export type CreateWorkspace = z.infer<typeof CreateWorkspaceObject>
 
+export const UpdateWorkspaceObject = z.object({
+  name: z
+    .string()
+    .min(5, {
+      message: 'Please insert at least 5 characters',
+    })
+    .max(50, {
+      message: 'The maximum length allowed it is 50 characters.',
+    })
+    .optional()
+});
+
+export type UpdateWorkspace = z.infer<typeof UpdateWorkspaceObject>
+
 export type Workspace = {
   id: string;
   name: string;
