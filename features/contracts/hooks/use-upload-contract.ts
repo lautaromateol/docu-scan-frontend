0fa471c0cee8761912099afe181094d5c774827f
lautaromateol <lautaromateol@gmail.com>
2025-07-31 12:26:02 -0300

    update: redirect added to upload hook

--- features/contracts/hooks/use-upload-contract.ts
+++ features/contracts/hooks/use-upload-contract.ts
@@ -1,27 +1,37 @@
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
 import { uploadContract as uploadContractAction } from "../actions/upload-contract";
+import { Contract } from "../types";
 
 type ResponseType = {
   statusCode: number;
-  text: string;
-}
+  contract: Contract;
+};
 
-export function useUploadContract() {
-  const { mutate: uploadContract, isPending: isUploadingContract } = useMutation<ResponseType, Error, File>({
-    mutationFn: async (file) => {
-      const data = await uploadContractAction(file)
+export function useUploadContract(workspaceId: string) {
+  const router = useRouter();
 
-      if(data.statusCode !== 200) {
-        throw new Error(data.message ?? "There was an error uploading your contract.")
-      }
+  const { mutate: uploadContract, isPending: isUploadingContract } =
+    useMutation<ResponseType, Error, File>({
+      mutationFn: async (file) => {
+        const data = await uploadContractAction({ file, workspaceId });
 
-      return data
-    },
-    onError: (error) => {
-      toast.error(error.message)
-    } 
-  })
+        if (data.statusCode !== 200) {
+          throw new Error("There was an error uploading your contract.");
+        }
 
-  return { uploadContract, isUploadingContract }
+        return data;
+      },
+      onSuccess: (data) => {
+        if (data.statusCode === 200) {
+          router.push(`/dashboard/workspace/${workspaceId}/contracts/${data.contract.id}`);
+        }
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    });
+
+  return { uploadContract, isUploadingContract };
 }
