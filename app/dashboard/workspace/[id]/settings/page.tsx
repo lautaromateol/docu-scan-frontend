ae0bc1608ebc1b371047a0f116b033d05c21e633
lautaromateol <lautaromateol@gmail.com>
2025-07-31 12:25:19 -0300

    update: settings page header

--- app/dashboard/workspace/[id]/settings/page.tsx
+++ app/dashboard/workspace/[id]/settings/page.tsx
@@ -1,8 +1,8 @@
-import { Separator } from "@/components/ui/separator";
 import { getWorkspace } from "@/features/workspaces/actions/get-workspace";
 import { DangerZone } from "@/features/workspaces/components/danger-zone";
 import { InviteMembers } from "@/features/workspaces/components/invite-members";
 import { UpdateWorkspace } from "@/features/workspaces/components/update-workspace";
+import { Settings } from "lucide-react";
 
 export default async function WorkspaceSettingsPage({ params }: any) {
   const { id } = await params;
@@ -14,14 +14,17 @@ export default async function WorkspaceSettingsPage({ params }: any) {
 
   return (
     <div className="space-y-6">
-      <div className="space-y-0.5">
-        <h1 className="text-2xl font-semibold">Settings</h1>
-        <p className="text-base font-light">
-          Update your workspace name, invite members and more.
+      <div className="flex flex-col items-center space-y-3 mx-auto w-1/2">
+        <div className="rounded-lg shadow-md p-4 bg-gradient-to-r from-indigo-700 to-indigo-900 w-auto">
+          <Settings className="text-white" />
+        </div>
+        <h1 className="text-3xl font-bold">Workspace Settings</h1>
+        <p className="text-xl text-gray-600 text-center">
+          Customize your workspace, manage team access, and configure security
+          settings with enterprise-grade controls.
         </p>
       </div>
-      <Separator className="w-full" />
-      <UpdateWorkspace workspace={workspace} /> 
+      <UpdateWorkspace workspace={workspace} />
       <InviteMembers workspace={workspace} />
       <DangerZone workspace={workspace} />
     </div>
