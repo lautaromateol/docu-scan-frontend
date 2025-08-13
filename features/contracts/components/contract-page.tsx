2f235960a9110358ab63015b662b126bec391a3f
lautaromateol <lautaromateol@gmail.com>
2025-08-13 15:08:49 -0300

    update: breadcrumbs added to contract page

--- features/contracts/components/contract-page.tsx
+++ features/contracts/components/contract-page.tsx
@@ -1,10 +1,12 @@
 import { Contract } from "../types";
+import { ContractPageBreadcrumbs } from "./contract-page-breadcrumbs";
 import { ContractPageHeader } from "./contract-page-header";
 import { ContractPageTabs } from "./contract-page-tabs";
 
 export function ContractPage({ contract }: { contract: Contract }) {
   return (
     <div className="flex flex-col space-y-8">
+      <ContractPageBreadcrumbs contract={contract} />
       <ContractPageHeader contract={contract} />
       <ContractPageTabs contract={contract} />
     </div>
