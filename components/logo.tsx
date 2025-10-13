8f15293deefda57d9425df84420cf7d053c3edd6
lautaromateol <lautaromateol@gmail.com>
2025-10-13 12:59:16 -0300

    update: logo variant added

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,13 +1,29 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
-export function Logo() {
+export function Logo({
+  variant = "default",
+}: {
+  variant?: "default" | "landing";
+}) {
   return (
     <div className="flex items-center gap-x-2">
       <div className="relative h-8 w-12">
-        <Image src="/logo.svg" alt="Docuscan logo" fill />
+        <Image
+          src={variant === "default" ? "/logo.svg" : "/logo-indigo.svg"}
+          alt="Docuscan logo"
+          fill
+        />
       </div>
-      <p className="text-2xl font-bold text-white">Docuscan</p>
+      <p
+        className={cn(
+          "text-3xl font-extrabold",
+          variant === "landing" ? "text-indigo-700" : "text-white"
+        )}
+      >
+        Docuscan
+      </p>
     </div>
   );
 }
