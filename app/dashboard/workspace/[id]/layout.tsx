24e895e44339b29e926cd7d75c032fa22d0b654d
lautaromateol <lautaromateol@gmail.com>
2025-10-13 13:10:33 -0300

    update: dashboard layout validations

--- app/dashboard/workspace/[id]/layout.tsx
+++ app/dashboard/workspace/[id]/layout.tsx
@@ -1,17 +1,44 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Navbar } from "./components/navbar";
 import { AppSidebar } from "./components/sidebar";
+import { getUser } from "@/features/users/actions/get-user";
+import { redirect } from "next/navigation";
+import { getMembers } from "@/features/members/actions/get-members";
 
 export default async function DashboardIdLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: any;
 }) {
+  const { id } = await params;
+
+  const user = await getUser();
+  const members = await getMembers(id);
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const me = members.find((member) => member.userId === user.id);
+
+  if (!me) {
+    throw new Error();
+  }
+
   return (
     <SidebarProvider>
       <div className="w-full flex">
         <div className="hidden lg:block">
-          <AppSidebar />
+          <AppSidebar
+            user={{
+              email: user.email,
+              name: user.name,
+              image: user.pictureUrl,
+              role: me.role,
+            }}
+          />
         </div>
         <div className="flex flex-col w-full">
           <Navbar />
