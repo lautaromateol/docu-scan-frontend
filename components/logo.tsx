6bac5e138d168fcaa9e966da5402f0e7d81b7101
lautaromateol <lautaromateol@gmail.com>
2025-10-13 22:33:50 -0300

    update: docuscan removed from logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,4 @@
-import { cn } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
 
 export function Logo({
   variant = "default",
@@ -8,22 +6,12 @@ export function Logo({
   variant?: "default" | "landing";
 }) {
   return (
-    <div className="flex items-center gap-x-2">
-      <div className="relative h-8 w-12">
-        <Image
-          src={variant === "default" ? "/logo.svg" : "/logo-indigo.svg"}
-          alt="Docuscan logo"
-          fill
-        />
-      </div>
-      <p
-        className={cn(
-          "text-3xl font-extrabold",
-          variant === "landing" ? "text-indigo-700" : "text-white"
-        )}
-      >
-        Docuscan
-      </p>
+    <div className="relative h-8 w-12">
+      <Image
+        src={variant === "default" ? "/logo.svg" : "/logo-indigo.svg"}
+        alt="Docuscan logo"
+        fill
+      />
     </div>
   );
 }
